45997dba60401d144d557370654b2e07bb285c90
pranav2910 <pranavsai2910@gmail.com>
2025-03-25 07:09:46 +0000

    comit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Spring Boot Application...");
+		System.out.println("Starting Spring Boot iujiujioijApplication...");
 		SpringApplication.run(Main.class, args);
-	}
+	} 
 
 }
